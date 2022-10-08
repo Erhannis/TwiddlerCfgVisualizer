46a8dbd16b891ddb6b5f0897eefaee5fe10860ce
erhannis <eyeillus@gmail.com>
2022-10-08 19:07:09 -0400

    Create frames by modifier key, added regex highlighting, probably some other stuff

--- src/main/java/com/erhannis/twiddlercfgvisualizer/Main.java
+++ src/main/java/com/erhannis/twiddlercfgvisualizer/Main.java
@@ -27,7 +27,7 @@ import java.util.stream.Collectors;
  * @author erhannis
  */
 public class Main extends javax.swing.JFrame {
-    private HashMap<Integer, ImageFrame> renderFrames = new HashMap<>();
+    private HashMap<Integer, ChordsFrame> chordsFrames = new HashMap<>();
     
     /**
      * Creates new form Main
@@ -71,11 +71,14 @@ public class Main extends javax.swing.JFrame {
         }
         System.out.println("loaded");
         
-        for (ImageFrame frame : renderFrames.values()) {
+        for (ChordsFrame frame : chordsFrames.values()) {
             if (frame != null) {
                 frame.setVisible(false);
             }
         }
+        
+        long nMultichords = tc.chords.stream().filter((c) -> c.isMultichord()).count();
+        labelInfo.setText("Config contains " + nMultichords + " multichord(s).");
         
         for (int i = 0b0000; i <= 0b1111; i++) {
             boolean num   = ((i & 0b0001) != 0);
@@ -85,105 +88,17 @@ public class Main extends javax.swing.JFrame {
             
             List<Chord> subset = tc.chords.stream().filter((c) -> (c.tbNum == num && c.tbCtrl == ctrl && c.tbAlt == alt && c.tbShift == shift)).collect(Collectors.toList());
             if (!subset.isEmpty()) {
-                ImageFrame frame = demandRenderFrame(i);
-                frame.setImage(renderChords(subset, tc.strings));
+                ChordsFrame frame = demandChordsFrame(i);
+                frame.setChords(subset, tc.strings);
                 frame.setVisible(true);
             }
         }
     }
-
-    private static final float FONT = 10;
-    private static final Color CO = MeUtils.interpolateColors(Color.GRAY, Color.WHITE, 0.5);
-    private static final Color CL = MeUtils.interpolateColors(Color.RED, Color.WHITE, 0.5);
-    private static final Color CM = MeUtils.interpolateColors(Color.BLUE, Color.WHITE, 0.5);
-    private static final Color CR = MeUtils.interpolateColors(Color.GREEN, Color.WHITE, 0.5);
-    private static final Color[] COLORS = {CO, CL, CM, CR};
-    private static final double SX = 1600;
-    private static final double SY = 600;
-    private static final double DY = SY / 4;
     
-    private BufferedImage renderChords(List<Chord> chords, ArrayList<String> stringTable) {
-        double DX = SX / (4*4*4*3);
-        
-        BufferedImage bi = new BufferedImage((int)SX, (int)SY, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = (Graphics2D) bi.getGraphics();
-        
-        g.setBackground(Color.WHITE);
-        g.clearRect(0, 0, (int)SX, (int)SY);
-        g.setFont(g.getFont().deriveFont(FONT));
-        
-        subRender(g, 0, 0, SX / 4, 4);
-        
-        int multichords = 0;
-        g.setColor(Color.BLACK);
-        AffineTransform at = (AffineTransform)g.getTransform().clone();
-        for (Chord c : chords) {
-            if (c.isMultichord()) {
-                multichords++;
-                continue;
-            }
-            
-            double x = getChordX(c) * DX;
-            double y = (c.getLowestRow()-1) * DY;
-            
-            g.translate(x+1, y);
-            g.rotate(Math.PI/2);
-            g.drawString(c.getString(stringTable), 0, 0);
-            g.setTransform((AffineTransform)at.clone());
-            
-        }
-        System.out.println("Multichords unhandled: " + multichords);
-        
-        return bi;
-    }
-    
-    private int getChordX(Chord c) { // Does not work for multichords
-        int x = 0;
-
-        if (c.tbSp) x += 1*4*4*3;
-        if (c.tbE) x += 2*4*4*3;
-        if (c.tbA) x += 3*4*4*3;
-        
-        if (c.tbDel) x += 1*4*3;
-        if (c.tbF) x += 2*4*3;
-        if (c.tbB) x += 3*4*3;
-        
-        if (c.tbBs) x += 1*3;
-        if (c.tbG) x += 2*3;
-        if (c.tbC) x += 3*3;
-        
-        if (c.tbEnt) x += 0;
-        if (c.tbH) x += 1;
-        if (c.tbD) x += 2;
-        
-        return x;
-    }
-    
-    private void subRender(Graphics2D g, double left, double top, double dx, int levels) {
-        if (levels == 0) {
-            return;
-        }
-        if (levels > 1) {
-            for (int i = 0; i < 4; i++) {
-                g.setColor(COLORS[i]);
-                g.fillRect((int)(left+i*dx), (int)top, (int)dx+1, (int)DY);
-            }
-        } else {
-            for (int i = 0; i < 3; i++) {
-                g.setColor(COLORS[i+1]);
-                g.fillRect((int)(left+i*dx), (int)top, (int)dx+1, (int)DY);
-            }
-        }
-        for (int i = 0; i < 4; i++) {
-            double div = (levels == 2) ? 3 : 4;
-            subRender(g, left + dx*i, top+DY, dx/div, levels-1);
-        }
-    }
-    
-    private ImageFrame demandRenderFrame(int modifiers) {
-        ImageFrame frame = renderFrames.get(modifiers);
+    private ChordsFrame demandChordsFrame(int modifiers) {
+        ChordsFrame frame = chordsFrames.get(modifiers);
         if (frame == null) {
-            frame = new ImageFrame();
+            frame = new ChordsFrame();
             
             boolean num   = ((modifiers & 0b0001) != 0);
             boolean ctrl  = ((modifiers & 0b0010) != 0);
@@ -192,7 +107,7 @@ public class Main extends javax.swing.JFrame {
 
             frame.setTitle("Modifiers: [" + (num ? "N" : "") + (ctrl ? "C" : "") + (alt ? "A" : "") + (shift ? "S" : "") + "]");
             
-            renderFrames.put(modifiers, frame);
+            chordsFrames.put(modifiers, frame);
         }
         return frame;
     }
@@ -208,11 +123,14 @@ public class Main extends javax.swing.JFrame {
 
         jPanel3 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        labelInfo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Twiddler Cfg Visualizer");
 
         jLabel1.setText("Drag a v5 Twiddler .cfg file here to render it.");
+
+        labelInfo.setText("[No cfg loaded]");
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -220,7 +138,9 @@ public class Main extends javax.swing.JFrame {
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(labelInfo))
                 .addContainerGap(133, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
@@ -228,7 +148,9 @@ public class Main extends javax.swing.JFrame {
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addContainerGap(274, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(labelInfo)
+                .addContainerGap(242, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -260,5 +182,6 @@ public class Main extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JLabel labelInfo;
     // End of variables declaration//GEN-END:variables
 }
